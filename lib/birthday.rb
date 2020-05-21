afdc2134a648015266aa8df09a677f51a3b8a26b
Brian Matheny <bwmatheny@gmail.com>
2020-05-21 14:38:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each {|name, age|
     if age < 13
-      puts "Happy Birthday #{name}! you are now #{age} years old!"
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
     else
       puts "You are too old for this."
     end}
